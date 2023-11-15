788a16b07ec4bf85e80f049ab1715b97b849bfc8
MattyCi <mjciaravino@gmail.com>
2023-11-14 21:32:15 -0500

    commit extra examples

--- src/main/java/com/example/UserRegistrationValidator.java
+++ src/main/java/com/example/UserRegistrationValidator.java
@@ -6,12 +6,21 @@ import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.validation.validator.constraints.ConstraintValidator;
 import io.micronaut.validation.validator.constraints.ConstraintValidatorContext;
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
 @Introspected
 class UserRegistrationValidator implements ConstraintValidator<ValidUserRegistration, UserRegistrationRequest> {
 
+    // @Inject
+    /*SomeService someService;
+
+    @Inject
+    UserRegistrationValidator(SomeService someService) {
+        this.someService = someService;
+    }
+*/
     @Override
     public boolean isValid(UserRegistrationRequest value, @NonNull AnnotationValue<ValidUserRegistration> annotationMetadata,
                            @NonNull ConstraintValidatorContext context) {
